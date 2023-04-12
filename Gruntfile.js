09180a12149e779340367c8c1085aa062066bcfa
decaffeinate <mauricio.szabo@gmail.com>
2023-04-11 21:30:00 -0300

    decaffeinate: Run post-processing cleanups on Gruntfile.coffee and 11 other files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,11 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
  */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
     shell: {
       'update-atomdoc': {
@@ -15,8 +17,8 @@ module.exports = function(grunt) {
         }
       }
     }
-  });
+  })
 
-  grunt.loadNpmTasks('grunt-shell');
-  return grunt.loadNpmTasks('grunt-atomdoc');
-};
+  grunt.loadNpmTasks('grunt-shell')
+  return grunt.loadNpmTasks('grunt-atomdoc')
+}
